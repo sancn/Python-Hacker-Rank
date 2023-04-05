fc2385aa07fb3c0cfb2bf0bd7df1fc797453278d
sancn <official.santu05@gmail.com>
2023-04-05 17:29:18 +0545

    function for leap year check

---
+++ leap_year.py
@@ -0,0 +1,12 @@
+year=int(input("Enter the year: "))
+def is_leap(year):
+    if year%400==0:
+        return True
+    
+    if year%100==0:
+        return False
+    
+    if year%4==0:
+        return True
+    return False
+print(is_leap(year))
